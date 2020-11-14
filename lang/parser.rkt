ca4e695e48d801d85fa95b9dc8bfa55a89c18074
Turab Jafri <trajafri@gmail.com>
2020-11-14 00:26:30 -0600

    use lexer for syntax-color

--- lang/parser.rkt
+++ lang/parser.rkt
@@ -2,7 +2,7 @@
 ;; Based off of https://github.com/dyoo/brainfudge and Racket docs
 ;; This module parses 1# and binds `ast` to 1# AST
 
-(require syntax/readerr)
+(require syntax/readerr syntax/parse)
 
 (provide (rename-out [parse-1sharp-exp read]
                      [parse-1sharp read-syntax])
@@ -222,32 +222,71 @@
 ; Any text prefixed by a ';' is colored 'comment
 (define colorer
   (λ (in offset mode)
-    (define-values (line column pos) (port-next-location in))
-    (define char (peek-char in))
-    (cond
-      [(eof-object? char) (read-char in) (values char 'eof #f #f #f 0 mode)]
-      [(char=? #\; char) (define comment (substring (read-line in) 1))
-                         (values comment 'comment #f pos (+ pos 1 (string-length comment)
-                                                            (if (eof-object? (peek-char in)) 0 1)) 0 mode)]
-      [(char-whitespace? char) (values (string (read-char in)) 'white-space #f pos (add1 pos) 0 mode)]
-      [else (read-char in)
-            (match mode
-              [`((instr ,ones ,sharps) . ,bool); we have atleast seen one '1'
-               (cond
-                 [(char=? #\1 char) (values (string char) (if bool 'keyword 'string)
-                                            #f pos (add1 pos) 0
-                                            `((instr ,(add1 ones) 0) . ,bool))]
-                 [(char=? #\# char)
-                  (define new-bool (if (= sharps 0) (not bool) bool))
-                  (define col (if new-bool 'string 'keyword))
-                  (values (string char) col #f pos (add1 pos) 0
-                          (if (= sharps 4) new-bool `((instr ,ones ,(add1 sharps)) . ,new-bool)))]
-                 [else (values (string char) 'error #f pos (add1 pos) 0 mode)])]
-              [bool; we expect to see a '1'
-               (cond
-                 [(char=? #\1 char) (values (string char) (if bool 'keyword 'string)
-                                            #f pos (add1 pos) 0 `((instr 1 0) . ,bool))]
-                 [else (values (string char) 'error #f pos (add1 pos) 0 mode)])])])))
+    (define tok (lex-1# #f in))
+    (define pos (syntax-position tok))
+    (define end-pos (if pos (+ 0 pos (syntax-span tok)) 0))
+    (syntax-parse tok
+      #:datum-literals(comment eof unknown whitespace)
+      [(eof) (values eof 'eof #f #f #f 0 mode)]
+      [(comment str) (values (syntax->datum #'str) 'comment #f pos end-pos 0 mode)]
+      [(whitespace) (values 'whitespace 'no-color #f pos end-pos 0 mode)]
+      [(unknown a) (values (string (syntax->datum #'a)) 'error #f pos end-pos 0 mode)]
+      [_ (match mode
+           [`((instr ,ones ,sharps) . ,bool); we have atleast seen one '1'
+            (syntax-parse tok
+              [#\1 (values "1" (if bool 'keyword 'string) #f pos end-pos 0
+                           `((instr ,(if (zero? sharps) (add1 ones) 1) 0) . ,bool))]
+              [#\#
+               (define new-bool (if (= sharps 0) (not bool) bool))
+               (define col (if new-bool 'string 'keyword))
+               (values "#" col #f pos end-pos 0
+                       (if (= sharps 4)
+                           new-bool
+                           `((instr ,ones ,(add1 sharps)) . ,new-bool)))])]
+           [bool; we expect to see a '1'
+            (syntax-parse tok
+              [#\1 (values "1" (if bool 'keyword 'string) #f pos end-pos 0 `((instr 1 0) . ,bool))]
+              [#\# (values "#" 'error #f pos end-pos 0 mode)])])])))
+(module+ test
+  (define test-color-port (open-input-string "
+        ;startWithACommentNewLine!
+1
+  1 #   ;1
+1 ####a #
+;lastLineNoNewLine!"))
+  (define (check-color? curr-mode exp-token exp-color exp-paren exp-pos exp-pos-end exp-mode)
+    (define-values (act-token act-color act-paren act-pos act-pos-end _ act-mode)
+      (colorer test-color-port 0 curr-mode))
+    (check-equal? act-token exp-token)
+    (check-eqv? act-color exp-color)
+    (check-eqv? act-paren exp-paren)
+    (check-eqv? act-pos exp-pos)
+    (check-eqv? act-pos-end exp-pos-end)
+    (check-equal? act-mode exp-mode))
+  
+  (check-color? #f 'whitespace 'no-color #f 1 10 #f)
+  (check-color? #f "startWithACommentNewLine!" 'comment #f 10 37 #f)
+  (check-color? #f "1" 'string #f 37 38 `((instr 1 0) . #f))
+  (check-color? `((instr 1 0) . #f) 'whitespace 'no-color #f 38 41 `((instr 1 0) . #f))
+  (check-color? `((instr 1 0) . #f) "1" 'string #f 41 42 `((instr 2 0) . #f))
+  (check-color? `((instr 2 0) . #f) 'whitespace 'no-color #f 42 43 `((instr 2 0) . #f))
+  (check-color? `((instr 2 0) . #f) "#" 'string #f 43 44 `((instr 2 1) . #t))
+  (check-color? `((instr 2 1) . #t) 'whitespace 'no-color #f 44 47 `((instr 2 1) . #t))
+  (check-color? `((instr 2 1) . #t) "1" 'comment #f 47 50 `((instr 2 1) . #t))
+  (check-color? `((instr 2 1) . #t) "1" 'keyword #f 50 51 `((instr 1 0) . #t))
+  (check-color? `((instr 1 0) . #t) 'whitespace 'no-color #f 51 52 `((instr 1 0) . #t))
+  (check-color? `((instr 1 0) . #t) "#" 'keyword #f 52 53 `((instr 1 1) . #f))
+  (check-color? `((instr 1 1) . #f) "#" 'keyword #f 53 54 `((instr 1 2) . #f))
+  (check-color? `((instr 1 2) . #f) "#" 'keyword #f 54 55 `((instr 1 3) . #f))
+  (check-color? `((instr 1 3) . #f) "#" 'keyword #f 55 56 `((instr 1 4) . #f))
+  (check-color? `((instr 1 4) . #f) "a" 'error #f 56 57 `((instr 1 4) . #f))
+  (check-color? `((instr 1 4) . #f) 'whitespace 'no-color #f 57 58 `((instr 1 4) . #f))
+  (check-color? `((instr 1 4) . #f) "#" 'keyword #f 58 59 #f)
+  (check-color? #f 'whitespace 'no-color #f 59 60 #f)
+  (check-color? #f "lastLineNoNewLine!" 'comment #f 60 79 #f)
+  (check-color? #f eof 'eof #f #f #f #f))
+
+
 
 (define (get-info in mod line col pos)
   (lambda (key default)
