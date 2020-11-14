988d22b64e4df7e14c3a65d114de819af44d1086
Turab Jafri <trajafri@gmail.com>
2020-11-14 03:18:47 -0600

    restructure/rename files, parse 1# using lexer

---
+++ lang/one-sharp/test.rkt
@@ -0,0 +1,30 @@
+#lang reader "parser.rkt"
+1#####
+
+
+1#
+;test
+1#
+
+
+1#1#
+;111####;domment?
+;11;dommentyh#
+;#####
+##
+;test
+#;howbout
+1#####1#1#
+;test
+1####
+;
+x
+1#
+;test
+hmm
+;wow?
+hmmmmmm?
+;HEY!
+;it worked finally
+p cool
+1#######
