a62a6d64db5d9580b2d90957b7a574d79082994e
Turab Jafri <trajafri@gmail.com>
2020-12-04 17:21:06 -0600

    make `one-sharp` a multi-collection

---
+++ info.rkt
@@ -0,0 +1,4 @@
+#lang info
+;https://beautifulracket.com/jsonic-3/the-info.rkt-file.html
+(define collection 'multi) ;https://plt.eecs.northwestern.edu/snapshots/current/pdf-doc/pkg.pdf#subsection.2.1
+(define version "0.1")
