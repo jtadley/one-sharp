912a24fc54f52958ce0f12bf0644ef4fefdc1ac2
Turab Jafri <trajafri@gmail.com>
2021-03-07 01:20:55 -0600

    add partial c:% support for formatting

--- one-sharp/main.rkt
+++ one-sharp/main.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (module reader racket
-  (require one-sharp/reader)
+  (require one-sharp/reader one-sharp/unparser)
   (provide read-syntax read get-info)
 
   ;################
@@ -16,4 +16,27 @@
         [(drracket:opt-out-toolbar-buttons)
          ; opt out of all usual DrRacket tool bar buttons
          '(drracket:syncheck debug-tool macro-stepper)]
+        [(drracket:keystrokes)
+         ; #lang's keystroke for formatting text when C-i is pressed
+         ; if parser throws an exception, the same text should be returned
+         `(("c:%" ,(λ (text% event%)
+                     (define lang-lit "#lang")
+                     (define one-sharp-lit "one-sharp")
+                     (define lang-code (send text% get-text))
+                     (define current-pos (send text% get-start-position))
+                     ; formatting the text
+                     ; need to remove #lang line, so let's remove that "properly"
+                     (define removed-lang
+                       (substring (string-trim (substring (string-trim lang-code)
+                                                          (string-length lang-lit)))
+                                  (string-length one-sharp-lit)))
+                     (define formatted-code (format removed-lang))
+                     (define final-code
+                       (if (string=? "" lang-code)
+                           ""
+                           (string-append lang-lit " " one-sharp-lit formatted-code)))
+                     ; now we edit the text% object
+                     (send text% clear)
+                     (send text% insert final-code 0 'same #f)
+                     (send text% set-position current-pos 'same #f #t 'default))))]
         [else default]))))
